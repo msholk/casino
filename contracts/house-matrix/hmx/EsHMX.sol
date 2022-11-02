3e0cd5e19e2a2a9d6ad3c393bd318a64910c9ccb
Max Sholk <Sholk@yahoo.com>
2022-11-02 09:49:59 +0100

    remove house matrix

--- contracts/house-matrix/hmx/EsHMX.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import "../tokens/MintableBaseToken.sol";
-
-
-contract EsHMX is MintableBaseToken {
-    constructor() MintableBaseToken('Escrowed HMX', 'EsHMX', 0) {}
-}
