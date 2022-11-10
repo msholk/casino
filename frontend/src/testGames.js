479dae24661a8dee9498dd75d0496bb23d6b0cf8
Max Sholk <Sholk@yahoo.com>
2022-11-10 18:35:17 +0100

    testGames

--- frontend/src/testGames.js
+++ frontend/src/testGames.js
@@ -15,41 +15,62 @@ function bet2WinFactor(betDet, winNum) {
   }
   return 0;
 }
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 function getRandomIntInclusive(min, max) {
   return Math.floor(((Math.random() * 1000000) % max) + 1); // The maximum is inclusive and the minimum is inclusive
 }
 function playManyGeames() {
   let GAMES_TO_PLAY = 10000000;
-  let haouseBalance = 10000;
+  let houseBalance = 10000;
   let initHouseBalance = 10000;
-  let playerBetsAmount = 200;
+  let playerBetsAmount = 100;
   let comission = playerBetsAmount * 0.0526 * 0.3;
-  let minB = haouseBalance;
-  let maxB = haouseBalance;
+  let minB = houseBalance;
+  let maxB = houseBalance;
+  let revenuBalance = houseBalance;
+  let revenue = 0;
   for (let iGame = 0; iGame < GAMES_TO_PLAY; iGame++) {
     let userBetsOn = getRandomIntInclusive(1, 33);
     let rouletteGives = getRandomIntInclusive(1, 38);
     const winFact = bet2WinFactor(userBetsOn, rouletteGives);
+
     if (winFact) {
-      haouseBalance -= 17;
+      houseBalance -= 17 * playerBetsAmount;
+      // const comissionAmount = 17 * playerBetsAmount * 0.0003;
+      // houseBalance -= comissionAmount;
+      // revenue += comissionAmount;
     } else {
-      haouseBalance += 1;
+      if (houseBalance > revenuBalance) {
+        houseBalance += playerBetsAmount * 0.7;
+        revenue += playerBetsAmount * 0.3;
+        revenuBalance = houseBalance;
+
+        // const comissionAmount = playerBetsAmount * 0.00026;
+        // houseBalance -= comissionAmount;
+        // revenue += comissionAmount;
+      } else {
+        houseBalance += playerBetsAmount;
+      }
     }
-    // haouseBalance -= comission;
+    // houseBalance -= comission;
     //   console.log(
-    //     `${iGame}: house balance:${haouseBalance} ${userBetsOn}:${rouletteGives}`
+    //     `${iGame}: house balance:${houseBalance} ${userBetsOn}:${rouletteGives}`
     //   );
-    minB = Math.min(minB, haouseBalance);
-    maxB = Math.max(maxB, haouseBalance);
+    minB = Math.min(minB, houseBalance);
+    maxB = Math.max(maxB, houseBalance);
   }
-  let netWin = haouseBalance - initHouseBalance;
+  let netWin = houseBalance - initHouseBalance;
   let allBetSum = playerBetsAmount * GAMES_TO_PLAY;
+  const profitOverAllBets = Math.round((netWin / allBetSum) * 10000) / 100;
+  revenue = numberWithCommas(Math.round(revenue));
+  houseBalance = numberWithCommas(Math.round(houseBalance));
+  minB = numberWithCommas(Math.round(minB));
+  maxB = numberWithCommas(Math.round(maxB));
+
   console.log(
-    `${Math.floor(minB)}:  ${Math.floor(maxB)}  ${Math.floor(
-      haouseBalance
-    )} ${comission} Profit over all bets:${
-      Math.round((netWin / allBetSum) * 10000) / 100
-    }%`
+    `minB:${minB}:  maxB:${maxB}  houseBalance:${houseBalance} ${comission} Profit over all bets:${profitOverAllBets}% revenue:${revenue}`
   );
 }
 for (let index = 0; index < 10; index++) {
