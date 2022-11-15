c910e1c9d3bf263f3a63a07553eed75744d1df92
astro <57165451+Astr0-G@users.noreply.github.com>
2022-11-14 17:11:24 -0800

    Delete reactCombineLandingPage/build directory

--- reactCombineLandingPage/build/static/js/563.c910c3e5.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkbank_project=self.webpackChunkbank_project||[]).push([[563],{6563:function(){}}]);
--- reactCombineLandingPage/build/static/js/942.8bafe252.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkbank_project=self.webpackChunkbank_project||[]).push([[942],{4654:function(){},2361:function(){},4616:function(){}}]);
