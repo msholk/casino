53adbc34034622aaf5020afeb6a3151dbcae6a1d
Max Sholk <Sholk@yahoo.com>
2022-11-17 14:22:38 +0100

    parseEther

--- reactCombineLandingPage/src/libs/stakerLib.js
+++ reactCombineLandingPage/src/libs/stakerLib.js
@@ -23,7 +23,8 @@ export const reclaimHLP = async ({
       let myAddress = await signer.getAddress();
       console.log("provider signer...", myAddress);
 
-      const txn = await stakerContract.reclaimHLP(claimAmount);
+      const bn = ethers.utils.parseEther(claimAmount);
+      const txn = await stakerContract.reclaimHLP(bn);
       console.log("Withdrawing money...");
       setBusy("Withdrawing money...");
       await txn.wait();
